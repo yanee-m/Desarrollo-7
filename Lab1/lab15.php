ec3a6d775343fc8438405d29cbf7c4c30aac7980
Yanellys M <101084126+yanee-m@users.noreply.github.com>
2022-09-06 23:44:46 -0500

    Cambios a Lab 1 y Lab 2

--- Lab1/lab15.php
+++ Lab1/lab15.php
@@ -2,24 +2,25 @@
   <head>
     <title>Laboratorio 1.5</title>
   </head>
-    <body>
+  <body>
     <?php
-    define ('TAM', 10);
-    echo <table border=1>;
-    $n = 1;
-    for ($n1 = 1; $n1<=TAM; $n1++)
-    {
-    if ($n1 % 2 == 0)
-    echo "<tr bgcolor=#bdc3d6>";
-    else
-    echo "<tr>";
-    for ($n2 = 1; $n2<=TAM; $n2++){
-      echo "<td>", $n, "</td>";
-      $n=$n+1;
-    }
-    echo "</tr>";
-  }
-  echo "</table>";
+        define ('TAM', 10);
+        echo "<table border=1>";
+        $n = 1;
+        for ($n1 = 1; $n1<=TAM; $n1++)
+        {
+        if ($n1 % 2 == 0)
+        echo "<tr bgcolor=#bdc3d6>";
+        else
+        echo "<tr>";
+        for ($n2 = 1; $n2<=TAM; $n2++)
+        {
+          echo "<td>", $n, "</td>";
+          $n=$n+1;
+        }
+        echo "</tr>";
+      }
+      echo "</table>";
     ?>
-    </body>
+  </body>
 </html>
